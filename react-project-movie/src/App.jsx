6c95af2847187259b2522c2f68a1ab1d94bf41ee
omaroughriss <134494017+omaroughriss@users.noreply.github.com>
2025-01-15 14:22:50 +0100

    Update App.jsx

--- react-project-movie/src/App.jsx
+++ react-project-movie/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieDetail';
+import Wishlist from './components/Whishlist';
+import Navbar from './components/Navbar';
+import { WishlistProvider } from './context/WishlistProvider';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <WishlistProvider>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<MovieList />} />
+                    <Route path="/movie/:id" element={<MovieDetail />} />
+                    <Route path="/wishlist" element={<Wishlist />} />
+                </Routes>
+            </Router>
+        </WishlistProvider>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
